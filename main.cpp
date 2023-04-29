b259c909f7acd908f4822d08aae21eb0d888304f
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-04-29 12:11:06 -0600

    Create menu with options

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void showMenu() {
+    cout << " --------Menu---------" << endl;
+    cout << " 1. Check balance " << endl;
+    cout << " 2. Deposit " << endl;
+    cout << " 3. Withdraw " << endl;
+    cout << " ---------------------" << endl;
+};
+
+int main() {
+
+    // functionality - check balance, deposit, withdraw, show menu
+
+    showMenu();
+
+
+
+    return 0;
+}
