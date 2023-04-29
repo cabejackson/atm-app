f0a66b155da04d3bde3e4f3cb4d3b83d49869d23
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-04-29 12:45:18 -0600

    Add exit option

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ void showMenu() {
     cout << " 1. Check balance " << endl;
     cout << " 2. Deposit " << endl;
     cout << " 3. Withdraw " << endl;
+    cout << " 4. Exit " << endl;
     cout << " ---------------------" << endl;
 };
 
@@ -18,35 +19,37 @@ int main() {
     double depositAmount;
     double withdrawAmount;
 
-    showMenu();
-    cout << "Please choose an option (1, 2, 3): ";
-    cin >> option;
+    do {
+        showMenu();
+        cout << "Please choose an option (1, 2, 3): ";
+        cin >> option;
 
-    switch(option) {
-        case 1:
-            cout << "Balance is: $" << balance << endl;
-            break;
-        case 2:
-            cout << "Deposit amount: $";
-            cin >> depositAmount;
-            if (depositAmount > 0) { 
-            balance += depositAmount;
-            } else {
-                cout << "Deposit must be greater than 0" << endl;
-            }
-            cout << "The Balance is now: $" << balance << endl;
-            break;
-        case 3:
-            cout << "How much would you like to withdraw?: $";
-            cin >> withdrawAmount;
-            if (withdrawAmount <= balance) {
-                balance -= withdrawAmount;
+        switch(option) {
+            case 1:
+                cout << "Balance is: $" << balance << endl;
+                break;
+            case 2:
+                cout << "Deposit amount: $";
+                cin >> depositAmount;
+                if (depositAmount > 0) { 
+                balance += depositAmount;
+                } else {
+                    cout << "Deposit must be greater than 0" << endl;
+                }
                 cout << "The Balance is now: $" << balance << endl;
-            } else {
-                cout << "Insufficient funds." << endl;
-            }
-            break;
-    };
+                break;
+            case 3:
+                cout << "How much would you like to withdraw?: $";
+                cin >> withdrawAmount;
+                if (withdrawAmount <= balance) {
+                    balance -= withdrawAmount;
+                    cout << "The Balance is now: $" << balance << endl;
+                } else {
+                    cout << "Insufficient funds." << endl;
+                }
+                break;
+        };
+    } while(option != 4);
 
     return 0;
 }
