ff161d638af5c74152ce317b6f4427a266515c86
Caleb Jackson <jacksoncaleb.calebjackson@gmail.com>
2023-05-29 18:52:47 -0600

    Add tests for withdraw and cleanup commented out code

--- main.cpp
+++ main.cpp
@@ -25,12 +25,10 @@ void deposit(Account& account, double amount) {
     else 
         cout << "Deposit must be greater than 0" << endl;
     displayBalance(account);
-    
-    // cout << endl;
 };
 
 TEST_CASE("Deposit increases total balance"){
-    Account account;
+    Account account; //create an instance of account
     account.balance = 0; //initialize the balance 
     
     //Deposit $100
@@ -41,19 +39,6 @@ TEST_CASE("Deposit increases total balance"){
     REQUIRE(account.balance == 100);
 };
 
-// void testDeposit(Account& account)
-// {
-//     // Account account; //create an instance of account
-//     account.balance = 0; //initialize the balance 
-
-//     //Deposit $100
-//     double depositAmount = 100;
-//     deposit(account, depositAmount);
-
-//     //assert that the balance is now $100
-//     assert(account.balance == 100);
-// };
-
 void withdraw(Account& account, double amount) {
     if (amount <= account.balance) {
         account.balance -= amount;
@@ -62,31 +47,31 @@ void withdraw(Account& account, double amount) {
         cout << "Insufficient funds." << endl;
 };
 
-// void testWithdrawl(Account& account)
-// {
-//     // Account account; //create an instance of account
-//     account.balance = 100; //initialize the balance 
+TEST_CASE("Withdraw decreases total balance"){
+    Account account; //create an instance of account
+    account.balance = 100; //initialize the balance
 
-//     //withdraw $100
-//     double withdrawAmount = 100;
-//     withdraw(account, withdrawAmount);
+    //withdraw $50
+    double withdrawAmount = 50;
+    withdraw(account, withdrawAmount);
 
-//     //assert that the balance is now $100
-//     assert(account.balance == 0);
-// };
+    //assert that the balance is now $50
+    REQUIRE(account.balance == 50);
 
-// void testWithdrawl2(Account& account)
-// {
-//     // Account account; //create an instance of account
-//     account.balance = 100; //initialize the balance 
+};
 
-//     //withdraw $100
-//     double withdrawAmount = 101;
-//     withdraw(account, withdrawAmount);
+TEST_CASE("Withdraw does not allow overdraw"){
+    Account account; //create an instance of account
+    account.balance = 100; //initialize the balance
 
-//     //assert that the balance is now $100
-//     assert(account.balance == 100);
-// };
+    //withdraw $101 (more than current balance)
+    double withdrawAmount = 101;
+    withdraw(account, withdrawAmount);
+
+    //assert that the balance is still 100
+    REQUIRE(account.balance == 100);
+
+};
 
 double getUserInput() {
     double amount;
@@ -126,14 +111,6 @@ int main(int argc, char* argv[]) {
                 break;
         };
     } while(menuOption != 4);
-
-    // tests
-    // cout << "test 1: " << endl;
-    // testDeposit(account);
-    // cout << "test 2: " << endl;
-    // testWithdrawl(account);
-    // cout << "test 3: " << endl;
-    // testWithdrawl2(account);
 
     return 0;
 }
